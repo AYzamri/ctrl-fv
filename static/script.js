5d802c294e0422df88f3352b9a6bfc168c799b38
shaked <shkdhazon@gmail.com>
2018-01-03 13:04:12 +0200

    first commit OMG

--- static/script.js
+++ static/script.js
@@ -8,7 +8,7 @@ var url = 'https://cfvtest.azurewebsites.net/';
 //var url = 'http://localhost:5000';
 var mainCtrl = app.controller('myCtrl', ['$scope','$http', function($scope,$http) {
               var ctrl = this;
-              ctrl.name = 'Ctrl-FV';
+              ctrl.name = 'Ctrl-FV!';
               ctrl.enqueue = function(){
                 var data = {message : ctrl.message}
                 $http.post(url+'/enqueue',data);
