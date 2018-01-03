47abcf4d90d5924d0d665c4a85d41f8c843c9317
shaked <shkdhazon@gmail.com>
2018-01-03 13:53:38 +0200

    first commit OMG

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request
-# from azure.storage.queue import QueueService
-# import json
+from azure.storage.queue import QueueService
+import json
+# import requests
 
 app = Flask(__name__, template_folder='Templates')
 
@@ -10,17 +11,23 @@ def hello_world():
     return render_template('index.html')
 
 
-# @app.route('/enqueue', methods=['POST'])
-# def enqueue():
-#     print('enqueue')
-#     data = json.loads(request.data.decode("utf-8"))
-#     message = data['message']
-#     accName = 'ctrlfvtest8165'
-#     accKey = 'LqjIxytc6pNz4MZkyEjqFxsi+ibW9A+XBA/634nQ8Di64hqINDNyiV+iVqE76yjYR7FNGpC6BY8yuzcExgNGAw=='
-#     queue_service = QueueService(account_name=accName, account_key=accKey)
-#     queue_service.put_message('q2test', message)
-#     return '', 200
+@app.route('/enqueue', methods=['POST'])
+def enqueue():
+    print('enqueue')
+    data = json.loads(request.data.decode("utf-8"))
+    message = data['message']
+    accName = 'ctrlfvtest8165'
+    queueName = 'q2test'
+    accKey = 'LqjIxytc6pNz4MZkyEjqFxsi+ibW9A+XBA/634nQ8Di64hqINDNyiV+iVqE76yjYR7FNGpC6BY8yuzcExgNGAw=='
 
+    # body = '<QueueMessage><MessageText>{0}</MessageText></QueueMessage>'.format(message)
+    # url = 'https://{0}.queue.core.windows.net/{1}/messages?'.format(
+    #     accName, queueName)
+    # requests.post(url=url, data=body)
+
+    queue_service = QueueService(account_name=accName, account_key=accKey)
+    queue_service.put_message(queueName, message)
+    return '', 200
 
 if __name__ == '__main__':
     app.run()
