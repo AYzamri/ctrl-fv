6d28b1d6494891994b5bc988900d5fa8af2c83a0
shaked <shkdhazon@gmail.com>
2018-01-03 16:08:08 +0200

    test w/ Q

--- main.py
+++ main.py
@@ -16,9 +16,9 @@ def enqueue():
     print('enqueue')
     data = json.loads(request.data.decode("utf-8"))
     message = data['message']
-    accName = 'ctrlfvtest8165'
-    queueName = 'q2test'
-    accKey = 'LqjIxytc6pNz4MZkyEjqFxsi+ibW9A+XBA/634nQ8Di64hqINDNyiV+iVqE76yjYR7FNGpC6BY8yuzcExgNGAw=='
+    accName = 'cfvftestac3f'
+    queueName = 'q3test'
+    accKey = '50hNdU1mXcoWTVMsB00FD55N92kq+/7Xmml43SfeKGby0n+63htAWR9i6AyEW9JglmDkF73T+qw5t5uA/LK0jg=='
 
     # body = '<QueueMessage><MessageText>{0}</MessageText></QueueMessage>'.format(message)
     # url = 'https://{0}.queue.core.windows.net/{1}/messages?'.format(
