2f28021dce285178184214b5b0fe80d54ce26d05
Shaked Hazon <shkdhazon@gmail.com>
2018-02-19 16:27:17 +0200

    Fixed upload?

--- server_logic.py
+++ server_logic.py
@@ -21,22 +21,21 @@ def upload_file_to_blob(name, file, container_name):
 
 
 def enqueue_message(qname, message):
-    encoded = message.decode()
-    # queue_service = QueueService(account_name=storage_acc_name, account_key=storage_acc_key)
-    # queue_service.put_message(qname, encoded)
+    queue_service = QueueService(account_name=storage_acc_name, account_key=storage_acc_key)
+    queue_service.put_message(qname, message)
 
 
-def upload_vid_meta_data(blobname, videoname, videodescription, user_id='none'):
-    server = 'cfvtest.database.windows.net'
-    database = 'cfvtest'
-    username = 'drasco'
-    password = 'testTest1'
-    driver = '{ODBC Driver 13 for SQL Server}'
-    table = 'VideosMetaData'
-    cnxn = pyodbc.connect(
-        'DRIVER=' + driver + ';PORT=1433;SERVER=' + server + ';PORT=1443;DATABASE=' + database + ';UID=' + username + ';PWD=' + password)
-    cursor = cnxn.cursor()
-    query = "INSERT INTO {0} (vid_id,title,description,userID) VALUES('{1}','{2}','{3}','{4}')"
-    query = query.format(table, blobname, videoname, videodescription, user_id)
-    cursor.execute(query)
-    cnxn.commit()
+    def upload_vid_meta_data(blobname, videoname, videodescription, user_id='none'):
+        server = 'cfvtest.database.windows.net'
+        database = 'cfvtest'
+        username = 'drasco'
+        password = 'testTest1'
+        driver = '{ODBC Driver 13 for SQL Server}'
+        table = 'VideosMetaData'
+        cnxn = pyodbc.connect(
+            'DRIVER=' + driver + ';PORT=1433;SERVER=' + server + ';PORT=1443;DATABASE=' + database + ';UID=' + username + ';PWD=' + password)
+        cursor = cnxn.cursor()
+        query = "INSERT INTO {0} (vid_id,title,description,userID) VALUES('{1}','{2}','{3}','{4}')"
+        query = query.format(table, blobname, videoname, videodescription, user_id)
+        cursor.execute(query)
+        cnxn.commit()
