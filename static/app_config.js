56e4fa38e35c27eeddac54e0243784799aaa97d5
Shaked Hazon <shkdhazon@gmail.com>
2018-02-24 21:37:55 +0200

    fixed server

--- static/app_config.js
+++ static/app_config.js
@@ -4,5 +4,5 @@ var server = 'https://cfvtest.azurewebsites.net';
 var localhost = 'http://localhost:5000';
 var videoURL = "https://cfvtes9c07.blob.core.windows.net/videoscontainer";
 
-app.config['server'] = localhost;
+app.config['server'] = server;
 app.config['videoURL'] = videoURL;
