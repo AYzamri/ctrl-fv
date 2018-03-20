9f118017c94d1f047a4175e09d723cd847ff9432
Shaked Hazon <shkdhazon@gmail.com>
2018-03-20 13:16:53 +0200

    basic upload from UI w/ upload for large files

--- static/uploadVid.js
+++ static/uploadVid.js
@@ -1,8 +1,9 @@
 var app = angular.module('myApp');
 var server = app.config['server'];
 
-app.controller('uploadVidCtrl', ['$http', '$scope', 'upload', 'userService', function ($http, $scope, upload, userService) {
+app.controller('uploadVidCtrl', ['$http', '$scope', '$interval', 'upload', 'userService', function ($http, $scope, $interval, upload, userService) {
     var ctrl = this;
+    ctrl.isUploading = false;
     ctrl.url = server + '/video';
 
     ctrl.hello = 'uploadVidCtrl is up';
@@ -38,20 +39,60 @@ app.controller('uploadVidCtrl', ['$http', '$scope', 'upload', 'userService', fun
 
     };
     ctrl.doUpload = function () {
-        upload({
-            url: ctrl.url,
-            method: 'POST',
-            data: {
-                video: ctrl.vidArray[0],
-                videoName: ctrl.videoName,
-                videoDescription: ctrl.videoDescription,
-                transcript: ctrl.transArray[0],
-                user: userService.User.email
+        ctrl.progress = 0;
+
+        function refreshProgress()
+        {
+            $interval(function () {
+                if (ctrl.isUploading)
+                {
+                    var progress = speedSummary.getCompletePercent();
+                    ctrl.progress = progress;
+                }
+                else
+                    $interval.cancel();
+            }, 1000);
+        }
+
+        // If one file has been selected in the HTML file input element
+        var file = ctrl.vidArray[0];
+        var fileName = ctrl.videoName + '.mp4';
+
+        var blobUri = 'https://' + 'cfvtes9c07' + '.blob.core.windows.net';
+        var sas = '?sv=2017-07-29&ss=b&srt=sco&sp=rwac&se=2018-03-20T18:02:46Z&st=2018-03-20T10:02:46Z&spr=https&sig=YVrkCOS7ynYhLTG2GBJjEhUH9ff1%2FPPQ1rlhWhxnLvM%3D';
+        var blobService = AzureStorage.Blob.createBlobServiceWithSas(blobUri, sas);
+
+        var customBlockSize = file.size > 1024 * 1024 * 32 ? 1024 * 1024 * 4 : 1024 * 512;
+        blobService.singleBlobPutThresholdInBytes = customBlockSize;
+
+        var finishedOrError = false;
+        ctrl.isUploading = true;
+        var speedSummary = blobService.createBlockBlobFromBrowserFile('videoscontainer', fileName, file, {blockSize: customBlockSize}, function (error, result, response) {
+            ctrl.isUploading = false;
+            if (error)
+            {
+                window.alert(error)
             }
-        }).then(function () {
-            window.alert('added video to archive')
-        }, function (error) {
-            window.alert('Error uploading video')
-        })
+            else
+            {
+                window.alert('success!')
+            }
+        });
+        refreshProgress();
+        // upload({
+        //     url: ctrl.url,
+        //     method: 'POST',
+        //     data: {
+        //         video: ctrl.vidArray[0],
+        //         videoName: ctrl.videoName,
+        //         videoDescription: ctrl.videoDescription,
+        //         transcript: ctrl.transArray[0],
+        //         user: userService.User.email
+        //     }
+        // }).then(function () {
+        //     window.alert('added video to archive')
+        // }, function (error) {
+        //     window.alert('Error uploading video')
+        // })
     }
 }]);
