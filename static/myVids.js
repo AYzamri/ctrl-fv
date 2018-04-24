2ca950b59e73c26c3a410d8d224b9d8ffeed8e9f
Adam <adam.yzamri@gmail.com>
2018-04-24 14:30:56 +0300

    improved thumbnail

--- static/myVids.js
+++ static/myVids.js
@@ -14,5 +14,9 @@ app.controller('myVidsCtrl', ['$http', '$location', 'userService', function ($ht
     ctrl.getVideos = function () {
         return ctrl.Service.User.videosData;
     }
+    ctrl.getVideoThumbNail = function (video_id){
+    var img_id = video_id.replace(".mp4",".png")
+    return "https://cfvtes9c07.blob.core.windows.net/image-container/"+img_id
+    }
 
 }]);
--- static/searchVid.js
+++ static/searchVid.js
@@ -18,7 +18,10 @@ app.controller('searchVidCtrl', ['$http', '$scope', function ($http, $scope)
             window.alert('Error finding videos');
         });
     };
-
+    ctrl.getVideoThumbNail = function (video_id){
+    var img_id = video_id.replace(".mp4",".png")
+    return "https://cfvtes9c07.blob.core.windows.net/image-container/"+img_id
+    }
 
     // func app trigger
     // ctrl.searchForVid = function ()
