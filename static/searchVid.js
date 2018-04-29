1f2beef124c8418dca506d123b98d2421aa9f7a3
Shaked Hazon <shkdhazon@gmail.com>
2018-04-29 17:14:11 +0300

    added number of videos found

--- static/searchVid.js
+++ static/searchVid.js
@@ -1,26 +1,23 @@
 var app = angular.module('myApp');
 var server = app.config['server'];
 
-app.controller('searchVidCtrl', ['$http', '$scope', function ($http, $scope)
-{
+app.controller('searchVidCtrl', ['$http', '$scope', function ($http, $scope) {
     var ctrl = this;
     ctrl.searchVal = "";
-
+    ctrl.searchFinished = false;
     ctrl.vid_search_results = [];
 
-    ctrl.searchForVid = function ()
-    {
-        $http.get(server + '/searchForVideo?searchterm=' + encodeURI(ctrl.searchVal)).then(function (results)
-        {
+    ctrl.searchForVid = function () {
+        $http.get(server + '/searchForVideo?searchterm=' + encodeURI(ctrl.searchVal)).then(function (results) {
+            ctrl.searchFinished = true;
             ctrl.vid_search_results = results.data;
-        }).catch(function (err)
-        {
+        }).catch(function (err) {
             window.alert('Error finding videos');
         });
     };
-    ctrl.getVideoThumbNail = function (video_id){
-    var img_id = video_id.replace(".mp4",".png")
-    return "https://cfvtes9c07.blob.core.windows.net/image-container/"+img_id
+    ctrl.getVideoThumbNail = function (video_id) {
+        var img_id = video_id.replace(".mp4", ".png");
+        return "https://cfvtes9c07.blob.core.windows.net/image-container/" + img_id
     }
 
     // func app trigger
@@ -41,17 +38,13 @@ app.controller('searchVidCtrl', ['$http', '$scope', function ($http, $scope)
     // };
 
 }]);
-app.filter('secondsToDateTime', [function ()
-{
-    return function (seconds)
-    {
+app.filter('secondsToDateTime', [function () {
+    return function (seconds) {
         return new Date(1970, 0, 1).setSeconds(seconds);
     };
 }]);
-app.filter("trustUrl", ['$sce', function ($sce)
-{
-    return function (recordingUrl)
-    {
+app.filter("trustUrl", ['$sce', function ($sce) {
+    return function (recordingUrl) {
         return $sce.trustAsResourceUrl(recordingUrl);
     };
 }]);
