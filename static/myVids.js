71e1f228d05e9ea709e955debe333c77122e12b0
Shaked Hazon <shkdhazon@gmail.com>
2018-05-09 14:54:02 +0300

    show message for no videos in myVideos

--- static/myVids.js
+++ static/myVids.js
@@ -1,36 +1,31 @@
 var app = angular.module('myApp');
 var server = app.config['server'];
 
-app.controller('myVidsCtrl', ['$http', '$location', '$mdToast', 'userService', function ($http, $location, $mdToast, userService)
-{
+app.controller('myVidsCtrl', ['$http', '$location', '$mdToast', 'userService', function ($http, $location, $mdToast, userService) {
     var ctrl = this;
-
+    ctrl.retrievedVideos = false;
     ctrl.Service = userService;
 
-    ctrl.init = function ()
-    {
+    ctrl.init = function () {
         // Refresh user data:
-        ctrl.Service.login(ctrl.Service.User.email, ctrl.Service.User.password);
+        ctrl.Service.login(ctrl.Service.User.email, ctrl.Service.User.password).then(function (value) {
+            ctrl.retrievedVideos = true;
+        });
     };
 
-    ctrl.getVideos = function ()
-    {
+    ctrl.getVideos = function () {
         return ctrl.Service.User.videosData;
     };
 
-    ctrl.removeVideoFromSystem = function (video_id, index)
-    {
+    ctrl.removeVideoFromSystem = function (video_id, index) {
         if (confirm("Are you sure want to delete this video? "))
         {
             ctrl.Service.User.videosData.splice(index, 1);
-            $http.get(server + '/removeVideoFromSystem?vid=' + encodeURI(video_id)).then(function (result)
-            {
-            }).catch(function (err)
-            {
+            $http.get(server + '/removeVideoFromSystem?vid=' + encodeURI(video_id)).then(function (result) {
+            }).catch(function (err) {
                 var toast = $mdToast.simple().textContent('Error').action('OK').highlightAction(true).position('bottom right');
 
-                $mdToast.show(toast).then(function (response)
-                {
+                $mdToast.show(toast).then(function (response) {
                     if (response === 'ok')
                         $mdDialog.hide()
                 });
