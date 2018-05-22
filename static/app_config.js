3455fd04c229d252d66bb6470cfd61dee34c704a
Shaked Hazon <shkdhazon@gmail.com>
2018-05-22 12:14:56 +0300

    updated app config

--- static/app_config.js
+++ static/app_config.js
@@ -4,6 +4,6 @@ var server = 'https://ctrl-fvideos.azurewebsites.net';
 var localhost = 'http://localhost:5000';
 var videoURL = "https://ctrlfvfunctionaa670.blob.core.windows.net/video-container";
 
-// app.config['server'] = server;
-app.config['server'] = localhost;
+app.config['server'] = server;
+// app.config['server'] = localhost;
 app.config['videoURL'] = videoURL;
